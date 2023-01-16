c0d949bcd7584cedaf373a9e3f90e1c1fc7e5850
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-16 14:11:28 -0300

    adicionando método para selecionar imagem

--- lib/components/user_image_picker.dart
+++ lib/components/user_image_picker.dart
@@ -1,20 +1,36 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   final void Function(File) onImagePick;
 
-  UserImagePicker({
-    Key? key,
-    required this.onImagePick
-  }) : super(key: key);
+  UserImagePicker({Key? key, required this.onImagePick}) : super(key: key);
 
   @override
   State<UserImagePicker> createState() => _UserImagePickerState();
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+
+      widget.onImagePick(_image!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,7 +38,24 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-        )
+          backgroundImage: _image != null ? FileImage(_image!) : null,
+        ),
+        TextButton(
+          onPressed: _pickImage,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.image,
+                color: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text('Adicionar Imagem')
+            ],
+          ),
+        ),
       ],
     );
   }
