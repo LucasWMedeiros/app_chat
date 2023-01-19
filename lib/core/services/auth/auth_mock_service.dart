b62cdacb7a8a4b4dbf5c561fef244b4cfaa501de
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-19 19:22:06 -0300

    configurando as opções de login mocado

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,17 +1,52 @@
 import 'dart:io';
+import 'dart:async';
+import 'dart:math';
 
 import 'package:app_chat/core/models/chat_user.dart';
 import 'package:app_chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  ChatUser? get currentUser{}
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _currentUser = null;
+    _updateUser(null);
+  });
 
-  Stream<ChatUser?> get UserChanges{}
+  ChatUser? get currentUser {
+    return _currentUser;
+  }
+
+  Stream<ChatUser?> get UserChanges {
+    return _userStream;
+  }
 
   Future<void> signup(
-      String nome, String email, String password, File image) async {}
+      String name, String email, String password, File image) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
 
-  Future<void> login(String email, String password) async {}
+    _users.putIfAbsent(email, () => newUser);
 
-  Future<void> logout() async {}
+    _updateUser(newUser);
+  }
+
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
