8dd8d1126da38fe5edaa6bc5abd250c4cb2068b1
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-19 19:24:53 -0300

    criando componente de seleção de tela
    criando componente com o objetivo de selecionar entre a tela de autenticação e a tela principal do app

---
+++ lib/screen/auth_or_app_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AuthOrAppScreen extends StatelessWidget {
+  const AuthOrAppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
