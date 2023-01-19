1255d219a17c98fb643485c66c5308ebe924d1d1
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-19 19:35:24 -0300

    configurando logica de autenticação
    Adicionando a logica de autenticação as telas

--- lib/screen/auth_or_app_screen.dart
+++ lib/screen/auth_or_app_screen.dart
@@ -1,4 +1,9 @@
 
+import 'package:app_chat/core/models/chat_user.dart';
+import 'package:app_chat/core/services/auth/auth_mock_service.dart';
+import 'package:app_chat/screen/auth_screen.dart';
+import 'package:app_chat/screen/chat_screen.dart';
+import 'package:app_chat/screen/loading_screen.dart';
 import 'package:flutter/material.dart';
 
 class AuthOrAppScreen extends StatelessWidget {
@@ -6,6 +11,17 @@ class AuthOrAppScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: StreamBuilder<ChatUser?>(
+        stream: AuthMockService().UserChanges,
+        builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return LoadingScreen();
+          } else {
+            return snapshot.hasData ? ChatScreen() : AuthScreen();
+          }
+        },
+      )
+    );
   }
 }
--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app_chat/components/auth_form.dart';
 import 'package:app_chat/core/models/auth_form_data.dart';
+import 'package:app_chat/core/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -14,43 +15,45 @@ class _AuthScreenState extends State<AuthScreen> {
 
   Future<void> _handleSubmit(AuthFormData formData) async {
     try {
-    setState(() =>_isLoading = true);
+      setState(() => _isLoading = true);
 
-    if(formData.isLogin) {
-
-    } else {
-
-    }
-
-    } catch(error) {
+      if (formData.isLogin) {
+        await AuthMockService().login(
+          formData.email,
+          formData.password,
+        );
+      } else {
+        await AuthMockService().signup(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
+      }
+    } catch (error) {
       //tratar erro
     } finally {
-    setState(() =>_isLoading = false);
-
+      setState(() => _isLoading = false);
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Stack(
-        children: [
-         Center(
+      body: Stack(children: [
+        Center(
           child: SingleChildScrollView(
             child: AuthForm(onSubmit: _handleSubmit),
           ),
         ),
-        if(_isLoading)
-        Container(
-          decoration: const BoxDecoration(
-            color: Color.fromRGBO(0, 0, 0, 0.5)
-          ),
-          child: const Center(child: CircularProgressIndicator()),
-        )
-        ]
-      ),
+        if (_isLoading)
+          Container(
+            decoration:
+                const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+            child: const Center(child: CircularProgressIndicator()),
+          )
+      ]),
     );
   }
 }
