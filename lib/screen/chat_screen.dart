33a82c1dc2db744a4c9e635727f4d0480a43020e
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-19 19:39:54 -0300

    registrando imagem no pubspec

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_chat/core/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,7 +7,18 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Chat page'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Chat page'),
+          TextButton(
+            onPressed: () {
+              AuthMockService().logout();
+            },
+            child: const Text('Logout'),
+          )
+        ],
+      ),
     );
   }
 }
