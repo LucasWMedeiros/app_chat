69de6e8b673cf2ea120b74c14c6d1875990e966e
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-20 14:06:50 -0300

    trabalhando na appBar da tela de chat
    Adicionando appBar na tela de chat e adicinando o botão para logout

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:app_chat/components/mensages.dart';
+import 'package:app_chat/components/new_mensage.dart';
 import 'package:app_chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -6,18 +8,48 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Chat page'),
-          TextButton(
-            onPressed: () {
-              AuthService().logout();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hunter Chat'),
+        centerTitle: true,
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children:const  [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(width: 10,),
+                      Text('Sair')
+                    ],
+                  ),
+                ),
+              )
+            ],
+            onChanged: (value) {
+              if(value == 'logout') {
+                AuthService().logout();
+              }
             },
-            child: const Text('Logout'),
           )
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMensage(),
+          ],
+        ),
       ),
     );
   }
