615fa965e426c2c7edaed8d6ba9d6053c491fb98
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-20 14:25:02 -0300

    criando classe mocada para os serviços de chat
    classe mocada para teste sem conexão com firebase

---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:app_chat/core/models/chat_menssage.dart';
+import 'package:app_chat/core/models/chat_user.dart';
+import 'package:app_chat/core/services/chat/chat_service.dart';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+  });
+
+  Stream<List<ChatMessage>> messagesStream() {
+    return _msgsStream;
+  }
+
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final _newMessage = ChatMessage(
+        id: Random().nextDouble().toString(),
+        text: text,
+        createdAt: DateTime.now(),
+        userId: user.id,
+        userName: user.name,
+        userImageURL: user.imageUrl,
+      );
+    _msgs.add(_newMessage);
+
+    _controller?.add(_msgs);
+
+    return _newMessage;
+  }
+}
