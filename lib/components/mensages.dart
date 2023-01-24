8c600884e1b12b41765113a1a0e674cf889c1f8a
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-24 13:38:55 -0300

    Começando a configurar a aprensentação de mensagens
    Começando a configurar a apresentação de mensagens no componente de mensagens.

--- lib/components/mensages.dart
+++ lib/components/mensages.dart
@@ -1,3 +1,5 @@
+import 'package:app_chat/core/models/chat_menssage.dart';
+import 'package:app_chat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +7,21 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mensagens'),
-    );
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (ctx, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator(),);
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('Sem Dados! Vamos conversar?'),);
+        } else {
+          final msg =snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msg.length,
+            itemBuilder: (ctx, i) => Text(msg[i].text),
+          );
+        }
+      });
   }
 }
