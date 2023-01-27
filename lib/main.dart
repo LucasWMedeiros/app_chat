beaa0368a2a249f4c6a972186a340dfa5c38ac27
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-01-27 13:16:22 -0300

    serviço de notificações
    não selecionado no commit anterior

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => PushNotificationService(),
+          create: (_) => ChatNotificationService(),
         ),
       ],
       child: MaterialApp(
