284a7e0f5ed400a0ec3828c157ff73bc90168719
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-02-06 12:35:54 -0300

    iniciando configuração de notificações

--- lib/screen/auth_or_app_screen.dart
+++ lib/screen/auth_or_app_screen.dart
@@ -1,16 +1,22 @@
 import 'package:app_chat/core/models/chat_user.dart';
 import 'package:app_chat/core/services/auth/auth_service.dart';
+import 'package:app_chat/core/services/notifications/chat_notifications_service.dart';
 import 'package:app_chat/screen/auth_screen.dart';
 import 'package:app_chat/screen/chat_screen.dart';
 import 'package:app_chat/screen/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppScreen extends StatelessWidget {
   const AuthOrAppScreen({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
@@ -21,18 +27,17 @@ class AuthOrAppScreen extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return LoadingScreen();
           } else {
-          return StreamBuilder<ChatUser?>(
-            stream: AuthService().UserChanges,
-            builder: (ctx, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return LoadingScreen();
-              } else {
-                return snapshot.hasData ? ChatScreen() : AuthScreen();
-              }
-            },
-          );
+            return StreamBuilder<ChatUser?>(
+              stream: AuthService().UserChanges,
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return LoadingScreen();
+                } else {
+                  return snapshot.hasData ? ChatScreen() : AuthScreen();
+                }
+              },
+            );
           }
-
         });
   }
 }
