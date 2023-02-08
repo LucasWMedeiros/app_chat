b995e2ae4a478a95e1b103778a532d41042159c4
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2023-02-08 07:58:41 -0300

    terminando de configurar o push notification no android

--- lib/core/services/notifications/chat_notifications_service.dart
+++ lib/core/services/notifications/chat_notifications_service.dart
@@ -2,7 +2,7 @@ import 'package:app_chat/core/models/chat_notification.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
-class ChatNotificationService with ChangeNotifier{
+class ChatNotificationService with ChangeNotifier {
   List<ChatNotification> _items = [];
 
   int get itemsCount {
@@ -13,19 +13,21 @@ class ChatNotificationService with ChangeNotifier{
     return [..._items];
   }
 
-  void add(ChatNotification notification){
+  void add(ChatNotification notification) {
     _items.add(notification);
     notifyListeners();
   }
 
-  void remove(int i){
+  void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
   }
 
   //Push Notification
   Future<void> init() async {
+    await _configureTerminated();
     await _configureForeground();
+    await _configureBackground();
   }
 
   Future<bool> get _isAuthorized async {
@@ -35,6 +37,30 @@ class ChatNotificationService with ChangeNotifier{
   }
 
   Future<void> _configureForeground() async {
-
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_menssageHandler);
+    }
   }
-}
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_menssageHandler);
+    }
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await _isAuthorized) {
+    RemoteMessage? initialMsg = await FirebaseMessaging.instance.getInitialMessage();
+    _menssageHandler(initialMsg);
+    }
+  }
+
+    void _menssageHandler(RemoteMessage? msg) {
+      if (msg == null || msg.notification == null) return;
+
+      add(ChatNotification(
+        title: msg.notification!.title ?? 'Não informado!',
+        body: msg.notification!.body ?? 'Não informado!',
+      ));
+    }
+  }
